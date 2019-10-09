cd4111e791c447258a6d05abebc84c3f7f6776e0
Vlad <vladyslav.ushatov@gmail.com>
2019-10-09 17:00:21 +0300

    1111

---
+++ Test Suites/1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f2760707-dc0b-4511-a02f-9435697aa61a</testSuiteGuid>
+   <testCaseLink>
+      <guid>d134388b-f5d8-47bf-870d-a82968535868</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
